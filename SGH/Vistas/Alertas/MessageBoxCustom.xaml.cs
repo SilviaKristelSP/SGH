2f2f3056a47407a111feac659016fbb8836274af
Rodrigo <sanchezcpvx@gmail.com>
2022-11-15 14:21:15 -0600

    Proyecto cliente     -LogIn     -Mensaje de notificación     -Mapeo base de datos Proyecto ServidorSGH     -Creación

---
+++ SGH/Vistas/Alertas/MessageBoxCustom.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SGH.Modelos;
+
+namespace SGH.Vistas.Alertas
+{
+    public partial class MessageBoxCustom : Window
+    {
+        private static string confirmationCode="";
+        private bool dialogResult = false;
+
+        public MessageBoxCustom() { }
+
+        public string GetCode()
+        {
+            return confirmationCode;
+        }
+
+        public bool GetDialog()
+        {
+            return dialogResult;
+        } 
+
+        public MessageBoxCustom(Alerta alerta)
+        {
+            InitializeComponent();            
+            if (alerta.messageLength.Equals("long"))
+                textBoxMessage.FontSize = 16;
+            else if (alerta.messageLength.Equals("medium"))
+                textBoxMessage.FontSize = 20;
+
+            textBoxMessage.Text = alerta.message;
+            
+            switch (alerta.type)
+            {
+
+                case MessageType.Info:
+                    txtTitle.Text = "Info";                    
+                    break;
+
+                case MessageType.Confirmation:
+                    txtTitle.Text = "Confirmación";                    
+                    break;
+
+                case MessageType.Success:                    
+                    txtTitle.Text = "Éxito";                    
+                    break;
+
+                case MessageType.Warning:
+                    txtTitle.Text = "Advertencia";                    
+                    break;
+
+                case MessageType.Error:                    
+                    txtTitle.Text = "Error";                    
+                    break;
+
+                case MessageType.Submit:                    
+                    break;
+
+            }
+
+            switch (alerta.buttons)
+            {               
+                case MessageButtons.Ok:                    
+                    buttonOk.Visibility = Visibility.Visible;
+                    buttonCancel.Visibility = Visibility.Collapsed;                                        
+                    buttonAccept.Visibility = Visibility.Collapsed;
+                    break;
+
+                case MessageButtons.Submit:                                      
+                    buttonOk.Visibility = Visibility.Collapsed;
+                    buttonCancel.Visibility = Visibility.Collapsed;                    
+                    buttonAccept.Visibility = Visibility.Collapsed;
+                    break;
+
+                case MessageButtons.AcceptCancel:
+                    buttonAccept.Visibility = Visibility.Visible;
+                    buttonOk.Visibility = Visibility.Collapsed;                                        
+                    break;
+
+            }            
+        }                  
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            dialogResult = true;           
+            this.Close();
+        }
+
+        private void ButtonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            dialogResult = true;
+            //confirmationCode = textBoxConfirmationCode.Text;
+            this.Close();
+            
+        }
+
+        private void ButtonAccept_Click(object sender, RoutedEventArgs e)
+        {
+            dialogResult = true;            
+            this.Close();
+        }
+       
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            dialogResult = false;           
+            this.Close();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            dialogResult = false;           
+            this.Close();
+        }              
+    }
+
+    public class Alerta
+    {
+        public string message { get; set; }
+        public MessageType type { get; set; }
+        public MessageButtons buttons { get; set; }
+        public string messageLength { get; set; }
+
+        public Alerta(string message, MessageType type, MessageButtons buttons, string messageLength)
+        {
+            this.message = message;
+            this.type = type;
+            this.buttons = buttons;
+            this.messageLength = messageLength;
+        }
+    }
+
+    public enum MessageType
+    {
+        Info,
+        Confirmation,
+        Success,
+        Warning,
+        Error,
+        Submit,
+    }
+    public enum MessageButtons
+    {                
+        Ok,
+        Submit,
+        AcceptCancel,
+    }
+}
---
+++ ServidorSGH/ServiceSGH.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ServidorSGH.Modelos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity.Core;
+using System.Data.SqlClient;
+using ServidorSGH.Modelos;
+
+namespace ServidorSGH
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IServiceSGH
+    {        
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public String GetRFCUsuario()
+        {
+
+            ServidorSGHContext servidorSGHContext = new ServidorSGHContext();            
+            Administrador administrador = servidorSGHContext.Administradors.Where(admin => admin.RFC == "123").FirstOrDefault();
+
+            return administrador.NombreCompleto;
+        }
+
+        public Administrador ExisteUsuario(string correo)
+        {
+            ServidorSGHContext sghContext = new ServidorSGHContext();
+            Administrador administrador =
+                    sghContext.Administradors.Where(
+                    admin => admin.Correo == correo
+                    ).FirstOrDefault();            
+
+            return administrador;
+        }
+    }
+}
