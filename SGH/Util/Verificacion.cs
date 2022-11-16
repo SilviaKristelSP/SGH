9995bfbcdcd78775118618034b5536b891cba410
Marlon MP <mrmarlonmp@gmail.com>
2022-11-16 10:05:39 -0600

    Creacion de la clase Verificacion
    - Agregue metodo VerificarNombre() que checa si un nombre cumole con un
      REGEX alfabetico
    - Agregue metodo VerificarExistencia, aunque aun no hace nada, es
      referencial
    - Agregue metodo VerificarDatos(), el cual verifica los datos en base a
      las dos funciones anteriores

---
+++ SGH/Util/Verificacion.cs
@@ -0,0 +1,33 @@
+﻿using SGH.Calificaciones;
+using System;
+using System.Text.RegularExpressions;
+
+namespace SGH.Util
+{
+
+    public static class Verificacion
+    {
+        public static bool VerificarNombre(String nombre)
+        {
+            Regex regex = new Regex("^[A-Za-z]+$");
+
+            if (regex.IsMatch(nombre)){
+                return true; }
+            return false;
+        }
+
+        public static bool VerificarExistencia(Estudiante estudiante)
+        {
+            return true;
+        }
+
+        public static bool VerificarDatos(Estudiante estudiante)
+        {
+            if (VerificarNombre(estudiante.nombre) && VerificarExistencia(estudiante))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
