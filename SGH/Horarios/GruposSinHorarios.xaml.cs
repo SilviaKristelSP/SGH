fd44a3653e67dd00a079d9a2eb02656596699290
Marlon MP <mrmarlonmp@gmail.com>
2022-11-24 00:07:52 -0600

    Creacion de la GUI-GruposSinHorarios
    - Esta GUI muestra en todos los grupos sin horario
    - Creacion del objeto: GrupoSinHorario para la organizacion de estos en
      el data grid

---
+++ SGH/Horarios/GruposSinHorarios.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SGH.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGH.Horarios
+{
+    /// <summary>
+    /// Interaction logic for GruposSinHorarios.xaml
+    /// </summary>
+    public partial class GruposSinHorarios : Window
+    {
+        public GruposSinHorarios()
+        {
+            InitializeComponent();
+            CargarGruposSinHorario();
+        }
+
+        private void CargarGruposSinHorario()
+        {
+            List<GrupoSinHorario> gruposSinHorario = new List<GrupoSinHorario>();
+            gruposSinHorario.Add(new GrupoSinHorario("5to", "A"));
+            dg_GruposSinHorario.ItemsSource = gruposSinHorario;
+        }
+
+        private void clickRegresar(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventana = new MainWindow();
+            this.Close();
+            ventana.Show();
+        }
+
+        private void clickGenerarHorario(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SGH/POCO/GrupoSinHorario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGH.POCO
+{
+    class GrupoSinHorario
+    {
+        private string semestre;
+        private string grupo;
+
+        public string Semestre { get => semestre; set => semestre = value; }
+        public string Grupo { get => grupo; set => grupo = value; }
+        public GrupoSinHorario(string semestre, string grupo)
+        {
+            this.semestre = semestre;
+            this.grupo = grupo;
+        }
+    }
+}
